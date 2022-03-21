a27ba5d787db6080562b6f64e900375cf184cf07
Arka ganteng <94210475+Arka250506@users.noreply.github.com>
2022-03-21 19:44:20 +0700

    Create mini-max sum.py

---
+++ mini-max sum.py
@@ -0,0 +1,9 @@
+def miniMaxSum(arr):  
+    arr.sort()
+    print (sum(arr[:-1]),sum (arr [1:]))
+              
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
